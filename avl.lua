a64717073d2e0aa1f61d4d28fefca59aaa41aee0
reisvictor1 <reisvictor448@gmail.com>
2020-12-08 14:26:16 -0300

    Fixed Add/print tree

--- avl.lua
+++ avl.lua
@@ -1,113 +1,157 @@
 
-Tree = {}
+local No = {no_dir = nil, no_esq = nil, valor = 0, height = 0};
 
-
-function Tree:new(atributos)
+function No:new(atributos)
     atributos = atributos or {};
-    setmetatable(atributos, self)
-    self._index = self;
-    return atributos
+    setmetatable(atributos, self);
+    self.__index = self;
+    return atributos;
 end
 
-function Tree:newNode(new_value)
-    return setmetatable({
-        value = new_value,
-        height = 0
-    }, Tree)
-end
+function No:getHeight(node)
+    
+    if(type(node) ~= "table") then
+        return 0;
+    end
 
-
-function Tree:getHeight(node)
     return node.height
 end
 
-function Tree:setHeight(node)
-    node.height = math.max(Tree:getHeight(node.right), Tree:getHeight(node.right)) + 1
+function No:setHeight(node)
+
+    if(type(node) ~= "table") then
+        return 1
+    end
+    node.height = math.max(No:getHeight(node.right), No:getHeight(node.right)) + 1
 end
 
-function Tree:getBalance(node)
-    return Tree:getHeight(node.right) - Tree:getHeight(node.left)
+-- function No:getBalance(node)
+--     return Arvore:getHeight(node.right) - Arvore:getHeight(node.left)
+-- end
+
+
+function No:insereNo(new_value)
+    if( (type(self.no_dir) ~= "table") and (new_value >= self.valor)) then
+        self.no_dir = No:new({valor = new_value});
+        No:setHeight(self.no_dir)
+        return;
+    end
+    if( (type(self.no_esq) ~= "table") and (new_value < self.valor)) then
+        self.no_esq = No:new({valor = new_value});
+        No:setHeight(self.no_esq)
+        return;
+    end
+
+    if(new_value >= self.valor) then
+        self.no_dir:insereNo(new_value);
+    else
+        self.no_esq:insereNo(new_value);
+    end
 end
 
-function Tree:rotateNode(sub_raiz, rot_side, oppo_side)
-    local pivo = sub_raiz[oppo_side]
-    sub_raiz[oppo_side] =  pivo[rot_side]
-    pivo[rot_side] = sub_raiz
-    sub_raiz, pivo = pivo, sub_raiz
-    Tree:setHeight(pivo)
-    Tree:setHeight(sub_raiz)
-    return sub_raiz
+
+
+
+Arvore = {raiz = nil, profundidade = 0}
+
+function Arvore:new(atributos)
+    atributos = atributos or {};
+    setmetatable(atributos, self);
+    self.__index = self;
+    return atributos;
 end
 
-function Tree:updateSubtree(sub_raiz)
-    Tree:setHeight(sub_raiz)
+-- function Arvore:rotateNode(sub_raiz, rot_side, oppo_side)
+--     local pivo = sub_raiz[oppo_side]
+--     sub_raiz[oppo_side] =  pivo[rot_side]
+--     pivo[rot_side] = sub_raiz
+--     sub_raiz, pivo = pivo, sub_raiz
+--     Arvore:setHeight(pivo)
+--     Arvore:setHeight(sub_raiz)
+--     return sub_raiz
+-- end
+
+function Arvore:updateSubArvore(sub_raiz)
+    Arvore:setHeight(sub_raiz)
     local rot_side, oppo_side, pivo, rotate_pivo
     local balance = getBalance(sub_raiz)
     if balance > 1 then
         pivo = sub_raiz.right
-        if Tree:getBalance(pivo) < 0 then rotate_pivo = true end
+        if Arvore:getBalance(pivo) < 0 then rotate_pivo = true end
         rot_side, oppo_side = 'left', 'right'
     elseif balance < -1 then
         pivo = sub_raiz.left 
-        if Tree:getBalance(pivo) > 0  then rotate_pivo = true end
+        if Arvore:getBalance(pivo) > 0  then rotate_pivo = true end
         rot_side, oppo_side = 'right', 'left'
     end
     if rot_side then
         if rotate_pivo then
-            sub_raiz[oppo_side] = Tree:rotateNode(pivo, oppo_side, rot_side)
+            sub_raiz[oppo_side] = Arvore:rotateNode(pivo, oppo_side, rot_side)
         end
-        sub_raiz = Tree:rotateNode(sub_raiz, rot_side, oppo_side)
+        sub_raiz = Arvore:rotateNode(sub_raiz, rot_side, oppo_side)
     end
     return sub_raiz
 end
 
-function Tree:add(self, new_value)
-   
-    if not self or not self.value then
-        self = newNode(new_value)
-    else
-        if new_value < self.value then
-            self.left = Tree:add(self.left,new_value)
-        elseif new_value > self.value then
-            self.right = Tree:add(self.right, new_value)
-        else new_value = nil end
-        Tree:updateSubtree(self)
+function Arvore:add(new_value)
+    if(type(new_value) ~= "number" and type(new_value) ~= "string") then
+        error("\27[33mErro: Tipo do dado não suportado!\27[0m")
     end
-end
 
-function Tree:remove(self, value)
-    local v = self.value
-    if v == value then
-        if not self.left or self.right then
-            return self.left or self.right
-        else
-            local sNode = self.right
-            while sNode.left do
-                sNode = sNode.left
-            end
-            self = Tree:remove(self, sNode.value)
-            self.value = sNode.value
-            return self
-        end
-    else 
-        if value < v then
-            self.left = Tree:remove(self.left, value)
-        else
-            self.right = Tree:remove(self.right, value)
-        end
+    if (type(self.raiz) ~= "table") then 
+      self.raiz = No:new({ valor = new_value })
+      No:setHeight(self.raiz)
+      return
     end
-    return updateSubtree(self)
+
+    self.raiz:insereNo(new_value);
+
+    --Arvore:updateSubArvore(self)
 end
 
-function Tree:printTree(self)
-    if self then
-        Tree:printTree(self.left)
-        print(self.value)
-        Tree:printTree(self.right)
+-- function Arvore:remove(value)
+--     local v = self.value
+--     if v == value then
+--         if not self.left or self.right then
+--             return self.left or self.right
+--         else
+--             local sNode = self.right
+--             while sNode.left do
+--                 sNode = sNode.left
+--             end
+--             self = Arvore:remove(self, sNode.value)
+--             self.value = sNode.value
+--             return self
+--         end
+--     else 
+--         if value < v then
+--             self.left = Arvore:remove(self.left, value)
+--         else
+--             self.right = Arvore:remove(self.right, value)
+--         end
+--     end
+--     return updateSubArvore(self)
+-- end
+
+function No:print()
+    if(type(self.no_esq) =="table") then
+      io.write(self.no_esq:print());
     end
-end
 
+    io.write(string.format("valor:%s height:%s\n", self.valor, self.height));
 
-function Tree:free()
-    
-end
+    if(type(self.no_dir) == "table") then
+      io.write(self.no_dir:print());
+    end
+
+  end
+
+function Arvore:print(no_atual)
+    if(self.raiz == 0) then error("\27[33mErro: Arvore vazia!\27[0m") end
+    if(type(no_atual) == "nil") then
+      no_atual = self.raiz;
+    end
+    print(no_atual:print());
+  end
+
+return Arvore;
