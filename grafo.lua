c08b683c2c22de6da40d7876303421bc29913e69
matrigato <51022834+matrigato@users.noreply.github.com>
2020-12-28 19:31:33 -0300

    grafo.lua: Added print

--- grafo.lua
+++ grafo.lua
@@ -22,7 +22,7 @@ function Grafo:addNo(index, valor)
         error("\27[33mErro: um nó com esse index já existe\27[0m")
     end
 
-    self.tamanho += 1
+    self.tamanho = self.tamanho + 1
     self.nos[index] = {adj = {}, valor = valor}
 
 end
@@ -36,7 +36,7 @@ function Grafo:removeNo(index)
         return
     end
 
-    self.tamanho -= 1
+    self.tamanho = self.tamanho - 1
     self.nos[index] = nil
     for k, v in pairs(self.nos) do
         self.nos[k].adj[index] = nil
@@ -65,6 +65,32 @@ function Grafo:removeEdge(src, dest)
     end
 
     self.nos[src].adj[dest] = nil
+end
+
+function Grafo:printAdjacent(index)
+    if(index == nil) then
+        error("\27[33mErro: index não pode ser vazio\27[0m")
+    end
+    if (self.nos[index] == nil) then
+        print(index .. ": inexistente")
+        return
+    end
+
+    print("[" .. index .. "]")
+    for k, v in pairs(self.nos[index].adj) do
+        print("\t[" .. k .. "] (" .. v .. ")")
+    end
+end
+
+function Grafo:print()
+    if(self.tamanho == 0) then
+        print("Grafo vazio")
+        return
+    end
+
+    for k, v in pairs(self.nos) do
+        self:printAdjacent(k)
+    end
 end
 
 return Grafo
