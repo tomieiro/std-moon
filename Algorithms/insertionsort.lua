1df55a310cf08e8fd8bdb08198ed10212404f197
Shawn Bragdon <115854106+obtusity@users.noreply.github.com>
2022-10-22 22:58:01 -0700

    Create some algorithms.

--- Algorithms/insertionsort.lua
+++ Algorithms/insertionsort.lua
@@ -1,1 +1,16 @@
- 
+return function(table)
+    local length = #table
+    for j = 2, length, 1 do
+        local value = table[j]
+        local i = j - 1
+        if type(value) ~= 'number' or i <= 0 then
+            break
+        end
+        while i > 0 and table[i] > value do
+            table[i + 1] = table[i]
+            i = i - 1
+        end
+        table[i + 1] = value
+    end
+    return table
+end
--- Algorithms/quicksort.lua
+++ Algorithms/quicksort.lua
@@ -1,1 +1,29 @@
- 
+return function(table)
+    if #table <= 1 then
+        return table
+    else
+        local len = #table
+        local left = {}
+        local right = {}
+        local newtable = {}
+        local pivot = table[len]
+        table[len] = nil
+        for i = 1, len, 1 do
+            local x = table[i]
+            if type(x) ~= 'number' then
+                break
+            end
+            if x <= pivot then
+                left[#left + 1] = x
+            elseif x > pivot then
+                right[#right + 1] = x
+            end
+        end
+        for _, t in pairs({algorithms.sort.quick(left), {pivot}, algorithms.sort.quick(right)}) do
+            for _, v in pairs(t) do
+                newtable[#newtable + 1] = v
+            end
+        end
+        return newtable
+    end
+end
