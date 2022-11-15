e592807b09895252b594a6de110abd4167549dd5
XDyao <56805700+XDyao@users.noreply.github.com>
2022-11-15 02:49:49 -0300

    Update na busca binária
    Inclusão do código para a busca binária em Lua.

--- Algorithms/binarysearch.lua
+++ Algorithms/binarysearch.lua
@@ -1,1 +1,22 @@
- 
+local function busca_binaria(array, valor)
+  local i = 1
+  local j = #array
+
+  while(i <= j) do
+    -- Divisao do vetor
+    local meio = (i + j) // 2
+    local val = array[meio]
+
+    -- Realiza a busca
+    if(val < valor) then
+      i = meio + 1
+    elseif(val > valor) then
+      j = meio - 1
+    else
+      return meio
+    end
+  end
+
+  -- Caso o numero buscado nao seja encontrado, ou o tamanho do vetor seja 0
+  return -1
+end
