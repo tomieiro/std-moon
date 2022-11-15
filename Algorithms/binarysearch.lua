c97a2f53047ae33d16ff3235b9bbb84b69edc187
XDyao <56805700+XDyao@users.noreply.github.com>
2022-11-15 03:16:19 -0300

    Algumas modificações na busca binária
    Adição de alguns comentários e pontuação coerente.

--- Algorithms/binarysearch.lua
+++ Algorithms/binarysearch.lua
@@ -1,22 +1,23 @@
 local function busca_binaria(array, valor)
-  local i = 1
-  local j = #array
+  local i = 1;
+  local j = #array;
 
   while(i <= j) do
     -- Divisao do vetor
-    local meio = (i + j) // 2
-    local val = array[meio]
+    local meio = (i + j) // 2;
+    local val = array[meio];
 
     -- Realiza a busca
     if(val < valor) then
-      i = meio + 1
+      i = meio + 1;
     elseif(val > valor) then
-      j = meio - 1
+      j = meio - 1;
     else
-      return meio
+      -- Valor encontrado na posicao retornada
+      return meio;
     end
   end
 
   -- Caso o numero buscado nao seja encontrado, ou o tamanho do vetor seja 0
-  return -1
+  return -1;
 end
