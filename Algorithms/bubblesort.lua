74b4af5e01b251d99c16ebfa986bf7c4685a6ccf
Fabio Dias <48016347+Coritiba019@users.noreply.github.com>
2022-12-21 23:21:55 -0300

    Create bubblesort.lua
    Closes #48

---
+++ Algorithms/bubblesort.lua
@@ -0,0 +1,11 @@
+function bubbleSort(arr)
+    local n = #arr
+    for i = 1, n do
+        for j = 1, n - i do
+            if arr[j] > arr[j + 1] then
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+            end
+        end
+    end
+    return arr
+end
