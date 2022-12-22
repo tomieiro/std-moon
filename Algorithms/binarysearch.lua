2247cbd4b1d6c982dd527abee606500f14eb27ea
Fabio Dias <48016347+Coritiba019@users.noreply.github.com>
2022-12-21 23:26:33 -0300

    Update binarysearch.lua
    Closes #44

--- Algorithms/binarysearch.lua
+++ Algorithms/binarysearch.lua
@@ -1,23 +1,15 @@
-local function busca_binaria(array, valor)
-  local i = 1;
-  local j = #array;
-
-  while(i <= j) do
-    -- Divisao do vetor
-    local meio = math.floor((i + j) / 2); -- Caso a divisao nao seja inteira, usamos apenas a parte inteira do resultado
-    local val = array[meio];
-
-    -- Realiza a busca
-    if(val < valor) then
-      i = meio + 1;
-    elseif(val > valor) then
-      j = meio - 1;
-    else
-      -- Valor encontrado na posicao retornada
-      return meio;
+function binary_search (array, value)
+    local first = 1
+    local last = #array
+    while first <= last do
+        local mid = math.floor((first + last) / 2)
+        if array[mid] < value then
+            first = mid + 1
+        elseif value < array[mid] then
+            last = mid - 1
+        else
+            return mid
+        end
     end
-  end
-
-  -- Caso o numero buscado nao seja encontrado, ou o tamanho do vetor seja 0
-  return -1;
+    return nil
 end
